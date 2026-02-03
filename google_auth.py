8da58e2edc6f027984889e4fdb00fd74b219d660
ASHAN9931 <myfbashank88@gmail.com>
2026-02-03 09:52:05 +0530

    Update login title and subtitle in google_auth.py

--- google_auth.py
+++ google_auth.py
@@ -615,8 +615,8 @@ def show_login_page(auth_url):
         </style>
         <div class="login-card">
             <div class="icon-wrapper">🔐</div>
-            <div class="login-title">WCPA Assistant</div>
-            <div class="login-subtitle">Welcome Wetland Conservation Policy Support Assistant</div>
+            <div class="login-title">Marsh Warden</div>
+            <div class="login-subtitle">Wetland Information & Conservation Policy support Assistant - Sri Lanka</div>
             <a href="{auth_url}" class="google-btn">
                 <svg class="google-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path fill="#fff" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
